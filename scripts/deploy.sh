e4efbcb518e8dabe68abe670c90ca23049d4be0b
Michał Składanowski <michal.skladanowski@melements.pl>
2020-11-27 16:11:52 +0100

    Fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -39,6 +39,9 @@ if [ $error == 0 ]; then
 fi
   
 cd "$PLUGIN_BUILDS_PATH"
+mkdir build 
+cd build
+
 # Remove any unzipped dir so we start from scratch
 rm -fR "$PLUGIN"
 # Unzip the built plugin
@@ -48,7 +51,7 @@ unzip -q -o "$ZIP_FILE"
 rm -fR svn
 
 # Checkout the SVN repo
-svn co -q "http://svn.wp-plugins.org/$PLUGIN" svn
+svn co -q "https://plugins.svn.wordpress.org/$PLUGIN" svn
 
 # Move out the trunk directory to a temp location
 mv svn/trunk ./svn-trunk
