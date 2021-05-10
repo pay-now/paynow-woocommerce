48202c65e5423e4ca416a4420d6922a78f9fff8d
Emil Leszczak <emil.leszczak@intramedium.pl>
2021-05-10 08:48:41 +0200

    Remove disable payment button

--- src/includes/gateways/class-wc-payment-gateway-pay-by-paynow-pl-pbl.php
+++ src/includes/gateways/class-wc-payment-gateway-pay-by-paynow-pl-pbl.php
@@ -18,12 +18,22 @@ class WC_Payment_Gateway_Pay_By_Paynow_PL_Pbl extends WC_Gateway_Pay_By_Paynow_P
 
 	public function payment_fields() {
 		wp_enqueue_style( $this->id . '_styles', WC_PAY_BY_PAYNOW_PL_PLUGIN_ASSETS . 'css/front.css', [], wc_pay_by_paynow_pl_plugin_version() );
-		wp_enqueue_script( $this->id . '_js', WC_PAY_BY_PAYNOW_PL_PLUGIN_ASSETS . 'js/front.js', [], wc_pay_by_paynow_pl_plugin_version(), true );
 		try {
 			$methods = $this->gateway->payment_methods();
 			include WC_PAY_BY_PAYNOW_PL_PLUGIN_FILE_PATH . 'includes/templates/pbls.phtml';
 		} catch ( PaynowException $exception ) {
 			WC_Pay_By_Paynow_PL_Logger::error( $exception->getMessage() );
 		}
+	}
+
+	public function process_payment( $order_id ) {
+		$payment_method_id = filter_input( INPUT_POST, 'paymentMethodId' );
+		if ( empty( $payment_method_id ) ) {
+			wc_add_notice( __( 'Please choose bank from the list to make payment', 'pay-by-paynow-pl' ), 'error' );
+
+			return;
+		}
+
+		return parent::process_payment( $order_id );
 	}
 }
