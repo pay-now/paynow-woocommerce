5bf3f33328d8a8e620132d0b5b5b8bf427e7e1fe
Emil Leszczak <emil.leszczak@intramedium.pl>
2021-05-19 15:49:41 +0200

    Add Google Pay (#90)

--- src/includes/gateways/class-wc-payment-gateway-pay-by-paynow-pl-blik.php
+++ src/includes/gateways/class-wc-payment-gateway-pay-by-paynow-pl-blik.php
@@ -5,7 +5,7 @@ class WC_Payment_Gateway_Pay_By_Paynow_PL_Blik extends WC_Gateway_Pay_By_Paynow_
 	public function __construct() {
 		$this->id                 = 'pay_by_paynow_pl_blik';
 		$this->title              = __( 'Blik payment', 'pay-by-paynow-pl' );
-		$this->description        = __( 'Secure BLIK, credit cards payments and fast online transfers provided by paynow.pl', 'pay-by-paynow-pl' );
+		$this->description        = __( 'Secure and fast payments provided by paynow.pl', 'pay-by-paynow-pl' );
 		$this->method_title       = __( 'paynow.pl - Blik payments', 'pay-by-paynow-pl' );
 		$this->method_description = __( 'Accept Blik payments with paynow.pl', 'pay-by-paynow-pl' );
 		$this->payment_method_id  = 2007;
---
+++ src/includes/gateways/class-wc-payment-gateway-pay-by-paynow-pl-google-pay.php
@@ -0,0 +1,14 @@
+<?php
+defined( 'ABSPATH' ) || exit();
+
+class WC_Payment_Gateway_Pay_By_Paynow_PL_Google_Pay extends WC_Gateway_Pay_By_Paynow_PL {
+	public function __construct() {
+		$this->id                 = 'pay_by_paynow_pl_google_pay';
+		$this->title              = __( 'Google Pay', 'pay-by-paynow-pl' );
+		$this->description        = __( 'Secure and fast payments provided by paynow.pl', 'pay-by-paynow-pl' );
+		$this->method_title       = __( 'paynow.pl - Google Pay payments', 'pay-by-paynow-pl' );
+		$this->method_description = __( 'Accept Google Pay payments with paynow.pl', 'pay-by-paynow-pl' );
+		$this->payment_method_id  = 2003;
+		parent::__construct();
+	}
+}
