6d59073fbaa5907c970b854a7bc17a8996f4733b
Emil Leszczak <emil.leszczak@intramedium.pl>
2021-07-05 10:09:08 +0200

    Fix/client initialisation for paynow gateway (#108)

--- src/includes/class-paynow-gateway.php
+++ src/includes/class-paynow-gateway.php
@@ -24,15 +24,19 @@ class Paynow_Gateway {
 	protected $signature_key;
 
 	public function __construct( array $settings ) {
-		$this->settings      = $settings;
-		$is_sandbox          = $settings['sandbox'] === "yes";
-		$api_key             = $is_sandbox ? $settings['sandbox_api_key'] : $settings['production_api_key'];
-		$this->signature_key = $settings['sandbox'] === "yes" ? $settings['sandbox_signature_key'] : $settings['production_signature_key'];
-		$this->client        = $this->client = new Client(
-			$api_key,
-			$this->signature_key,
-			$is_sandbox ? Environment::SANDBOX : Environment::PRODUCTION,
-			'Wordpress-' . get_bloginfo( 'version' ) . '/WooCommerce-' . WC()->version . '/Plugin-' . wc_pay_by_paynow_pl_plugin_version() );
+		if ( ! empty( $this->settings ) && isset( $settings['sandbox'] ) && isset( $settings['sandbox_api_key'] ) && isset( $settings['production_api_key'] ) ) {
+			$is_sandbox          = $settings['sandbox'] === "yes";
+			$api_key             = $is_sandbox ? $settings['sandbox_api_key'] : $settings['production_api_key'];
+			$this->signature_key = $settings['sandbox'] === "yes" ? $settings['sandbox_signature_key'] : $settings['production_signature_key'];
+
+			if ( $api_key && $this->signature_key ) {
+				$this->client = $this->client = new Client(
+					$api_key,
+					$this->signature_key,
+					$is_sandbox ? Environment::SANDBOX : Environment::PRODUCTION,
+					'Wordpress-' . get_bloginfo( 'version' ) . '/WooCommerce-' . WC()->version . '/Plugin-' . wc_pay_by_paynow_pl_plugin_version() );
+			}
+		}
 	}
 
 	/**
@@ -42,10 +46,14 @@ class Paynow_Gateway {
 	 * @param $return_url
 	 * @param null $payment_method_id
 	 *
-	 * @return Authorize
+	 * @return Authorize|void
 	 * @throws PaynowException
 	 */
 	public function payment_request( WC_Order $order, $return_url, $payment_method_id = null ) {
+		if ( ! $this->client ) {
+			return;
+		}
+
 		$currency     = WC_Pay_By_Paynow_PL_Helper::is_old_wc_version() ? $order->get_order_currency() : $order->get_currency();
 		$order_id     = WC_Pay_By_Paynow_PL_Helper::get_order_id( $order );
 		$billing_data = $order->get_address();
@@ -72,7 +80,7 @@ class Paynow_Gateway {
 				$product       = $item->get_product();
 				$order_items[] = [
 					'name'     => $product->get_title(),
-					'category' => $this->get_categories( $product->get_id() ),
+					'category' => WC_Pay_By_Paynow_PL_Helper::get_product_categories( $product->get_id() ),
 					'quantity' => $item->get_quantity(),
 					'price'    => WC_Pay_By_Paynow_PL_Helper::get_amount( WC_Pay_By_Paynow_PL_Helper::is_old_wc_version() ? wc_price( wc_get_price_including_tax( $product ) ) : $product->get_price_including_tax() )
 				];
@@ -90,32 +98,19 @@ class Paynow_Gateway {
 	}
 
 	/**
-	 * @param $product_id
-	 *
-	 * @return string|null
-	 */
-	private function get_categories( $product_id ) {
-		$terms = get_the_terms( $product_id, 'product_cat' );
-
-		$categories = [];
-		foreach ( $terms as $term ) {
-			$categories[] = $term->name;
-		}
-
-		return implode( ', ', $categories );
-	}
-
-	/**
 	 * Sends refund request
 	 *
 	 * @param $order_id
 	 * @param $payment_id
 	 * @param $amount
 	 *
-	 * @return Status
+	 * @return Status|void
 	 * @throws PaynowException
 	 */
 	public function refund_request( $order_id, $payment_id, $amount ) {
+		if ( ! $this->client ) {
+			return;
+		}
 		$refund = new Refund( $this->client );
 
 		return $refund->create(
@@ -131,6 +126,9 @@ class Paynow_Gateway {
 	 * @param $return_url
 	 */
 	public function send_shop_urls_configuration_request( $return_url ) {
+		if ( ! $this->client ) {
+			return;
+		}
 		try {
 			$shop_configuration = new ShopConfiguration( $this->client );
 			$shop_configuration->changeUrls( $return_url, WC_Pay_By_Paynow_PL_Helper::get_notification_url() );
@@ -147,13 +145,15 @@ class Paynow_Gateway {
 	}
 
 	/**
-	 * @return PaymentMethods
+	 * @return PaymentMethods|void
 	 * @throws PaynowException
 	 */
 	public function payment_methods() {
+		if ( ! $this->client ) {
+			return;
+		}
 		$payment = new Payment( $this->client );
 
 		return $payment->getPaymentMethods( get_woocommerce_currency(), WC_Pay_By_Paynow_PL_Helper::get_amount( WC()->cart->total ) );
 	}
-
 }
