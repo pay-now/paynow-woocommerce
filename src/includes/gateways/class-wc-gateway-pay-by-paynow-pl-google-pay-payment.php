612a5c8acdb6cdeef054b8ce670072b754d85aeb
Mateusz Twardowski <mateusz.twardowski@movecloser.pl>
2023-11-21 19:07:07 +0100

    remove empty lines

--- src/includes/gateways/class-wc-gateway-pay-by-paynow-pl-google-pay-payment.php
+++ src/includes/gateways/class-wc-gateway-pay-by-paynow-pl-google-pay-payment.php
@@ -22,7 +22,6 @@ class WC_Gateway_Pay_By_Paynow_PL_Google_Pay_Payment extends WC_Gateway_Pay_By_P
 	 * @return bool
 	 */
 	public function is_available(): bool {
-
 		return $this->is_payment_method_available( Type::GOOGLE_PAY ) &&
             WC_Pay_By_Paynow_PL_Helper::is_payment_method_available(WC_Gateway_Pay_By_Paynow_PL_Digital_Wallets_Payment::class);
 	}
