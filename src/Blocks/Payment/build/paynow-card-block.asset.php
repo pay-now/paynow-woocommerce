cd4c048b43b1890d42ba80f701461332e631987b
Mateusz Twardowski <mateusz.twardowski@movecloser.pl>
2024-02-20 11:47:23 +0100

    fix blocks code style

--- src/Blocks/Payment/build/paynow-card-block.asset.php
+++ src/Blocks/Payment/build/paynow-card-block.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('react'), 'version' => '6710366c52f1516041d3');
+<?php return array(
+	'dependencies' => array( 'react' ),
+	'version'      => '6710366c52f1516041d3',
+);
--- src/Blocks/Payment/build/paynow-pbl-block.asset.php
+++ src/Blocks/Payment/build/paynow-pbl-block.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('react'), 'version' => '73f50c24018bac626ab5');
+<?php return array(
+	'dependencies' => array( 'react' ),
+	'version'      => '73f50c24018bac626ab5',
+);
